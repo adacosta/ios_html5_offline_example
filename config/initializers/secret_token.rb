aff9ec860bd1f7ed8512bc1e8913c2ed31ece28b
Alan Da Costa <alandacosta@gmail.com>
2010-11-06 14:59:34 -0700

    Add base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = 'd6439fff00425b620b1d90c17efd8b6862fa9ebf38475d187349d581a4d3033280fa24aa2b7c6e2e88ed8f58ec1c79b2a4f546f7efd04350227319d96616833e'
